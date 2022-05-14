3a28c0b58ba64e831bf2d8042276f4bb4f428da8
EgorStrupinskiy <venik2405@yandex.ru>
2022-05-14 12:57:25 +0300

    ; deleted

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 var playerChoice = 0
-const playerOptions = ['paper', 'rock', 'scissors'];
+const playerOptions = ['paper', 'rock', 'scissors']
 const computerOptions = ['paper', 'rock', 'scissors']
 var computerChoice = 0
 var state = "offline"
@@ -13,48 +13,48 @@ let userInfo = {
     sessionStatus: ''
 }
 
-var socket;
+var socket
 
 document.getElementById("name_button").addEventListener("click", (e) => {
     if (document.getElementById("name").value === '') {
-        alert("Please, enter the name");
+        alert("Please, enter the name")
     } else {
         e.preventDefault()
-        socket = new WebSocket("ws://localhost:8080/websocket")
+        socket = new WebSocket("ws://26.192.55.23:8080/websocket")
         state = "online"
         socket.onopen = () => {
-            userInfo['sessionStatus'] = 'start';
-            userInfo['userName'] = document.getElementById("name").value;
-            socket.send(JSON.stringify(userInfo));
-            console.log("Socket is open");
+            userInfo['sessionStatus'] = 'start'
+            userInfo['userName'] = document.getElementById("name").value
+            socket.send(JSON.stringify(userInfo))
+            console.log("Socket is open")
         }
         document.getElementById("form").style.display = "none"
         document.getElementById("waiting_page").style.display = "flex"
         document.getElementById("pScore").textContent = "Your score: "
-        document.getElementById("game").scrollIntoView({ block: "center", behavior: "smooth" });
+        document.getElementById("game").scrollIntoView({ block: "center", behavior: "smooth" })
 
         socket.onmessage = (e) => {
-            console.log("Message received");
-            userInfo = JSON.parse(e.data);
+            console.log("Message received")
+            userInfo = JSON.parse(e.data)
             if (userInfo['sessionStatus'] === "start") {
-                console.log("start message");
+                console.log("start message")
                 document.getElementById("waiting_page").style.display = "none"
                 document.getElementById("game").style.display = "flex"
                 document.getElementById("cScore").textContent = userInfo.userName + ": "
 
             }
             if (userInfo['sessionStatus'] === "game") {
-                console.log("game message");
-                computerChoice = userInfo['userChoice'];
+                console.log("game message")
+                computerChoice = userInfo['userChoice']
                 document.getElementById("animation1").style.animation = 'example .4s 1'
                 document.getElementById("animation2").style.animation = 'example .4s 1'
             }
             if (userInfo['sessionStatus'] === "terminate") {
                 alert("Your opponent left")
-                console.log("terminate message");
+                console.log("terminate message")
                 document.getElementById("waiting_page").style.display = "flex"
                 document.getElementById("pScore").textContent = "Your score: "
-                document.getElementById("game").scrollIntoView({ block: "center", behavior: "smooth" });
+                document.getElementById("game").scrollIntoView({ block: "center", behavior: "smooth" })
                 document.getElementById("game").style.display = "none"
                 document.getElementById("rounds_left").style.display = "none"
                 document.getElementById("pScore").style.display = "none"
@@ -64,7 +64,7 @@ document.getElementById("name_button").addEventListener("click", (e) => {
                 document.getElementById("animation2").style.display = "none"
                 document.getElementById("hands_and_btn").style.display = "none"
                 document.getElementById("repeat_button").style.display = "none"
-            
+
                 document.getElementById("hands_and_btn").style.display = "block"
                 playerScore = 0
                 computerScore = 0
@@ -74,7 +74,7 @@ document.getElementById("name_button").addEventListener("click", (e) => {
 
 
         socket.onclose = () => {
-            console.log("Socket closed");
+            console.log("Socket closed")
         }
 
         //document.getElementById("form").style.display = "none"
@@ -86,21 +86,21 @@ document.getElementById("offline_button").addEventListener("click", (e) => {
 
     e.preventDefault()
 
-    userInfo['userName'] = "Computer";
+    userInfo['userName'] = "Computer"
     document.getElementById("form").style.display = "none"
-    console.log("start offline");
+    console.log("start offline")
     document.getElementById("pScore").textContent = "Your score: 0"
     document.getElementById("game").style.display = "flex"
     document.getElementById("cScore").textContent = userInfo.userName + ": 0"
 
-    document.getElementById("game").scrollIntoView({ block: "center", behavior: "smooth" });
+    document.getElementById("game").scrollIntoView({ block: "center", behavior: "smooth" })
 }
 )
 
 function clearBtns() {
-    document.getElementById("btn_paper_panel").style.background = "#ffed2b";
-    document.getElementById("btn_rock_panel").style.background = "#ffed2b";
-    document.getElementById("btn_scissors_panel").style.background = "#ffed2b";
+    document.getElementById("btn_paper_panel").style.background = "#ffed2b"
+    document.getElementById("btn_rock_panel").style.background = "#ffed2b"
+    document.getElementById("btn_scissors_panel").style.background = "#ffed2b"
 }
 
 
@@ -108,30 +108,30 @@ document.getElementById("elem1").addEventListener("click", (e) => {
     e.preventDefault()
     clearBtns()
     playerChoice = 0
-    document.getElementById("btn_paper_panel").style.background = "#753dc6";
+    document.getElementById("btn_paper_panel").style.background = "#753dc6"
     document.getElementById("btn_paper_panel").style.borderColor = "#fff"
     document.getElementById("game_button").style.display = "block"
-    document.getElementById("game_button").scrollIntoView({block: "center", behavior: "smooth"});
+    document.getElementById("game_button").scrollIntoView({ block: "center", behavior: "smooth" })
 })
 
 document.getElementById("elem2").addEventListener("click", (e) => {
     e.preventDefault()
     clearBtns()
     playerChoice = 1
-    document.getElementById("btn_rock_panel").style.background = "#753dc6";
+    document.getElementById("btn_rock_panel").style.background = "#753dc6"
     document.getElementById("btn_rock_panel").style.borderColor = "#fff"
     document.getElementById("game_button").style.display = "block"
-    document.getElementById("game_button").scrollIntoView({block: "center", behavior: "smooth"});
+    document.getElementById("game_button").scrollIntoView({ block: "center", behavior: "smooth" })
 })
 
 document.getElementById("elem3").addEventListener("click", (e) => {
     e.preventDefault()
     clearBtns()
     playerChoice = 2
-    document.getElementById("btn_scissors_panel").style.background = "#753dc6";
+    document.getElementById("btn_scissors_panel").style.background = "#753dc6"
     document.getElementById("btn_scissors_panel").style.borderColor = "#fff"
     document.getElementById("game_button").style.display = "block"
-    document.getElementById("game_button").scrollIntoView({block: "center", behavior: "smooth"});
+    document.getElementById("game_button").scrollIntoView({ block: "center", behavior: "smooth" })
 })
 
 
@@ -149,9 +149,9 @@ document.getElementById("game_button").addEventListener("click", (e) => {
 
     var lefthand = document.getElementById("animation1")
     var righthand = document.getElementById("animation2")
-    document.getElementById("left_hand").src = `./indexAssets/hands/leftWait.png`;
-    document.getElementById("right_hand").src = `./indexAssets/hands/rightWait.png`;
-    document.getElementById('result').textContent = "Wait...";
+    document.getElementById("left_hand").src = `./indexAssets/hands/leftWait.png`
+    document.getElementById("right_hand").src = `./indexAssets/hands/rightWait.png`
+    document.getElementById('result').textContent = "Wait..."
     var lefthand = document.getElementById("animation1")
     var righthand = document.getElementById("animation2")
     if (state === "offline") {
@@ -162,7 +162,7 @@ document.getElementById("game_button").addEventListener("click", (e) => {
         righthand.style.animation = 'example .4s infinite'
     }
 
-    document.getElementById("game").scrollIntoView({ block: "center", behavior: "smooth" });
+    document.getElementById("game").scrollIntoView({ block: "center", behavior: "smooth" })
 
 })
 
@@ -180,24 +180,24 @@ document.getElementById("repeat_button").addEventListener("click", (e) => {
 
 document.getElementById("leave_button").addEventListener("click", (e) => {
     e.preventDefault()
-    window.location.reload();
+    window.location.reload()
 
 })
 
 const game = () => {
     document.getElementById("animation1").addEventListener('animationend', function (e) {
-        document.getElementById("left_hand").src = `./indexAssets/hands/${playerOptions[playerChoice]}Left.png`;
-        document.getElementById("left_hand").style.transform = 'rotate(30deg)';
-        document.getElementById("right_hand").src = `./indexAssets/hands/${computerChoice}Right.png`;
-        document.getElementById("right_hand").style.transform = 'rotate(-30deg)';
-        // document.getElementById("repeat_button").scrollIntoView({block: "center", behavior: "smooth"});
-        document.getElementById('bottom').scrollIntoView(true);
+        document.getElementById("left_hand").src = `./indexAssets/hands/${playerOptions[playerChoice]}Left.png`
+        document.getElementById("left_hand").style.transform = 'rotate(30deg)'
+        document.getElementById("right_hand").src = `./indexAssets/hands/${computerChoice}Right.png`
+        document.getElementById("right_hand").style.transform = 'rotate(-30deg)'
+        // document.getElementById("repeat_button").scrollIntoView({block: "center", behavior: "smooth"})
+        document.getElementById('bottom').scrollIntoView(true)
 
         winner(playerOptions[playerChoice], computerChoice)
         if (moves == 10) {
-            gameOver(playerOptions);
+            gameOver(playerOptions)
         }
-    });
+    })
 
 
     // Function to
@@ -208,16 +208,16 @@ const game = () => {
         document.getElementById("game_button").addEventListener('click', function () {
             console.log("playerGame game_button event")
 
-            const movesLeft = document.getElementById('rounds_left');
-            moves++;
-            movesLeft.textContent = `Rounds Left: ${10 - moves}`;
+            const movesLeft = document.getElementById('rounds_left')
+            moves++
+            movesLeft.textContent = `Rounds Left: ${10 - moves}`
             if (state === "online") {
-                userInfo['sessionStatus'] = 'game';
-                userInfo['userChoice'] = playerOptions[playerChoice];
-                socket.send(JSON.stringify(userInfo));
+                userInfo['sessionStatus'] = 'game'
+                userInfo['userChoice'] = playerOptions[playerChoice]
+                socket.send(JSON.stringify(userInfo))
             } else {
-                const choiceNumber = Math.floor(Math.random() * 3);
-                computerChoice = computerOptions[choiceNumber];
+                const choiceNumber = Math.floor(Math.random() * 3)
+                computerChoice = computerOptions[choiceNumber]
             }
 
             // Function to check who wins
@@ -234,9 +234,9 @@ const game = () => {
     // Function to decide winner
 
     const winner = (player, computer) => {
-        const result = document.getElementById('result');
-        const playerScoreBoard = document.getElementById('pScore');
-        const opponentScoreBoard = document.getElementById('cScore');
+        const result = document.getElementById('result')
+        const playerScoreBoard = document.getElementById('pScore')
+        const opponentScoreBoard = document.getElementById('cScore')
         var resulString = ""
 
 
@@ -245,44 +245,44 @@ const game = () => {
         }
         else if (player == 'rock') {
             if (computer == 'paper') {
-                resulString = 'Opponent Won';
-                computerScore++;
+                resulString = 'Opponent Won'
+                computerScore++
             } else {
                 resulString = 'Player Won'
-                playerScore++;
+                playerScore++
             }
         }
         else if (player == 'scissors') {
             if (computer == 'rock') {
-                resulString = 'Opponent Won';
-                computerScore++;
+                resulString = 'Opponent Won'
+                computerScore++
             } else {
-                resulString = 'Player Won';
-                playerScore++;
+                resulString = 'Player Won'
+                playerScore++
             }
         }
         else if (player == 'paper') {
             if (computer == 'scissors') {
-                resulString = 'Opponent Won';
-                computerScore++;
+                resulString = 'Opponent Won'
+                computerScore++
             } else {
-                resulString = 'Player Won';
-                playerScore++;
+                resulString = 'Player Won'
+                playerScore++
             }
         }
         if (moves != 10)
             result.textContent = resulString
 
-        opponentScoreBoard.textContent = `${userInfo['userName']}: ${computerScore}`;
-        playerScoreBoard.textContent = `Your score: ${playerScore}`;
-        document.getElementById("repeat_button").style.display = "flex";
-        
-        document.getElementById('bottom').scrollIntoView({block: "center", behavior: "smooth"});
+        opponentScoreBoard.textContent = `${userInfo['userName']}: ${computerScore}`
+        playerScoreBoard.textContent = `Your score: ${playerScore}`
+        document.getElementById("repeat_button").style.display = "flex"
+
+        document.getElementById('bottom').scrollIntoView({ block: "center", behavior: "smooth" })
     }
 
 
     const gameOver = (playerOptions) => {
-        const result1 = document.getElementById("result");
+        const result1 = document.getElementById("result")
         result1.style.display = "flex"
         // document.getElementById("result").style.display = "none"
         // document.getElementById("animation1").style.display = "none"
@@ -290,26 +290,26 @@ const game = () => {
         // document.getElementById("hands_and_btn").style.display = "none"
         if (playerScore > computerScore) {
             result1.innerText = 'You Won The Game'
-            result1.style.color = '#308D46';
+            result1.style.color = '#308D46'
         }
         else if (playerScore < computerScore) {
-            result1.innerText = 'You Lost The Game';
-            result1.style.color = 'red';
+            result1.innerText = 'You Lost The Game'
+            result1.style.color = 'red'
         }
         else {
-            result1.innerText = 'Tie';
+            result1.innerText = 'Tie'
         }
-        document.getElementById("repeat_button").innerText = 'Restart';
+        document.getElementById("repeat_button").innerText = 'Restart'
         document.getElementById("repeat_button").addEventListener('click', () => {
-            window.location.reload();
+            window.location.reload()
         })
     }
 
 
     // Calling playGame function inside game
-    playGame();
+    playGame()
 
 }
 
 // Calling the game function
-game();
+game()
